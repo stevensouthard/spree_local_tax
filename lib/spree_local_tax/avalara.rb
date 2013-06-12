7c9494b653218486cf4fd6d598a881b503a6ca3e
Josh Nussbaum <joshnuss@gmail.com>
2013-06-12 12:38:26 -0400

    Adds InvoiceBuilder#add_destination

--- lib/spree_local_tax/avalara.rb
+++ lib/spree_local_tax/avalara.rb
@@ -4,6 +4,8 @@ module SpreeLocalTax::Avalara
   def generate(order)
     builder = InvoiceBuilder.new
     builder.customer = order.email if order.email.present?
+    address = order.bill_address
+    builder.add_destination(address.firstname, address.lastname, address.address1, address.address2, address.city, address.state.try(:abbr) || address.state_name, address.country.iso, address.zipcode)
     order.line_items.each do |line|
       variant = line.variant
       builder.add_line(variant.sku, variant.product.name, line.quantity, line.total)
--- spec/lib/avalara_spec.rb
+++ spec/lib/avalara_spec.rb
@@ -5,12 +5,14 @@ describe SpreeLocalTax::Avalara do
     let(:builder) { mock(:builder) }
 
     context "guest" do
-      let(:order) { mock(:order, email: nil, line_items: []) }
+      let(:address) { stub(:address, firstname: 'Wayne', lastname: 'Gretzky', address1: '123 Main St', address2: '#101', city: 'Toronto', state: stub(:state, abbr: 'ON'), country: stub(:country, iso: 'CA'), zipcode: 'H1H1H1')}
+      let(:order)   { stub(:order, email: nil, bill_address: address, line_items: []) }
 
       before do
         SpreeLocalTax::Avalara::InvoiceBuilder.should_receive(:new).and_return(builder)
         builder.should_receive(:invoice).and_return(:invoice)
         builder.should_not_receive(:customer=)
+        builder.should_receive(:add_destination)
       end
 
       subject { SpreeLocalTax::Avalara.generate(order) }
@@ -19,18 +21,21 @@ describe SpreeLocalTax::Avalara do
     end
 
     context "user" do
-      let(:product) { mock(:product, name: 'foo')}
-      let(:variant) { mock(:variant, sku: '1234', product: product)}
-      let(:line1)   { mock(:line, variant: variant, quantity: 2, total: 9.98) }
-      let(:line2)   { mock(:line, variant: variant, quantity: 3, total: 14.97) }
-      let(:order)   { mock(:order, email: 'wayne@gretzky.com', line_items: [line1, line2]) }
+      let(:address) { stub(:address, firstname: 'Wayne', lastname: 'Gretzky', address1: '123 Main St', address2: '#101', city: 'Toronto', state: stub(:state, abbr: 'ON'), country: stub(:country, iso: 'CA'), zipcode: 'H1H1H1')}
+      let(:product) { stub(:product, name: 'foo')}
+      let(:variant) { stub(:variant, sku: '1234', product: product)}
+      let(:line1)   { stub(:line, variant: variant, quantity: 2, total: 9.98) }
+      let(:line2)   { stub(:line, variant: variant, quantity: 3, total: 14.97) }
+      let(:order)   { stub(:order, email: 'wayne@gretzky.com', bill_address: address, line_items: [line1, line2]) }
 
       before do
         SpreeLocalTax::Avalara::InvoiceBuilder.should_receive(:new).and_return(builder)
+
         builder.should_receive(:invoice).and_return(:invoice)
         builder.should_receive(:customer=).with('wayne@gretzky.com')
         builder.should_receive(:add_line).with('1234', 'foo', 2, 9.98)
         builder.should_receive(:add_line).with('1234', 'foo', 3, 14.97)
+        builder.should_receive(:add_destination).with('Wayne', 'Gretzky', '123 Main St', '#101', "Toronto", "ON", "CA", "H1H1H1")
       end
 
       subject { SpreeLocalTax::Avalara.generate(order) }
