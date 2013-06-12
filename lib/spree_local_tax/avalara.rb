57cf9d3537c8bd5e375b6aaa3bd781ac7a4deda1
Josh Nussbaum <joshnuss@gmail.com>
2013-06-12 12:42:33 -0400

    Refactors SpreeLocalTax::Avalara module

--- lib/spree_local_tax/avalara.rb
+++ lib/spree_local_tax/avalara.rb
@@ -2,14 +2,18 @@ module SpreeLocalTax::Avalara
   extend self
 
   def generate(order)
-    builder = InvoiceBuilder.new
-    builder.customer = order.email if order.email.present?
     address = order.bill_address
+    builder = InvoiceBuilder.new
+
+    builder.customer = order.email if order.email.present?
+
     builder.add_destination(address.firstname, address.lastname, address.address1, address.address2, address.city, address.state.try(:abbr) || address.state_name, address.country.iso, address.zipcode)
+
     order.line_items.each do |line|
       variant = line.variant
       builder.add_line(variant.sku, variant.product.name, line.quantity, line.total)
     end
+
     builder.invoice
   end
 end
