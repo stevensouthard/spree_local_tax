0cb959729d0b009deb099e7ba07570c5620fa492
Josh Nussbaum <joshnuss@gmail.com>
2013-06-12 16:42:43 -0400

    Adds origin address to builder

--- lib/spree_local_tax/avalara/invoice_builder.rb
+++ lib/spree_local_tax/avalara/invoice_builder.rb
@@ -12,8 +12,12 @@ module SpreeLocalTax::Avalara
       @destination = ::Avalara::Request::Address.new(address_code: 'DESTINATION', line_1: line1, line_2: line2, city: city, region: region, country: country, postal_code: postal_code)
     end
 
+    def add_origin(line1, line2, city, region, country, postal_code)
+      @origin = ::Avalara::Request::Address.new(address_code: 'ORIGIN', line_1: line1, line_2: line2, city: city, region: region, country: country, postal_code: postal_code)
+    end
+
     def invoice
-      @invoice.addresses = [@destination]
+      @invoice.addresses = [@destination, @origin]
       @invoice
     end
   end
--- spec/lib/avalara/invoice_builder_spec.rb
+++ spec/lib/avalara/invoice_builder_spec.rb
@@ -7,6 +7,7 @@ describe SpreeLocalTax::Avalara::InvoiceBuilder do
     before do
       builder.customer = 'user@example.com'
       builder.add_destination('123 Main St.', '#101', 'Toronto', 'ON', 'CA', 'H1H1H1')
+      builder.add_origin('123 W Main St.', '#1000', 'Chicago', 'IL', 'US', '12345')
     end
 
     subject { builder.invoice }
@@ -23,6 +24,18 @@ describe SpreeLocalTax::Avalara::InvoiceBuilder do
       its(:Region)      { should == 'ON' }
       its(:Country)     { should == 'CA' }
       its(:PostalCode)  { should == 'H1H1H1' }
+    end
+
+    context "destination" do
+      subject { builder.invoice.Addresses[1] }
+
+      its(:AddressCode) { should == 'ORIGIN' }
+      its(:Line1)       { should == '123 W Main St.' }
+      its(:Line2)       { should == '#1000' }
+      its(:City)        { should == 'Chicago' }
+      its(:Region)      { should == 'IL' }
+      its(:Country)     { should == 'US' }
+      its(:PostalCode)  { should == '12345' }
     end
   end
 end
