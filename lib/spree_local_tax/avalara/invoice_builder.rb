e043427bc7c4b48fc7006acdfe14dded9ea81468
Josh Nussbaum <joshnuss@gmail.com>
2013-06-12 16:46:22 -0400

    Refactors builder addresses

--- lib/spree_local_tax/avalara/invoice_builder.rb
+++ lib/spree_local_tax/avalara/invoice_builder.rb
@@ -9,16 +9,22 @@ module SpreeLocalTax::Avalara
     end
 
     def add_destination(line1, line2, city, region, country, postal_code)
-      @destination = ::Avalara::Request::Address.new(address_code: 'DESTINATION', line_1: line1, line_2: line2, city: city, region: region, country: country, postal_code: postal_code)
+      @destination = address('DESTINATION', line1, line2, city, region, country, postal_code)
     end
 
     def add_origin(line1, line2, city, region, country, postal_code)
-      @origin = ::Avalara::Request::Address.new(address_code: 'ORIGIN', line_1: line1, line_2: line2, city: city, region: region, country: country, postal_code: postal_code)
+      @origin = address('ORIGIN', line1, line2, city, region, country, postal_code)
     end
 
     def invoice
       @invoice.addresses = [@destination, @origin]
       @invoice
+    end
+
+  private
+
+    def address(code, line1, line2, city, region, country, postal_code)
+      ::Avalara::Request::Address.new(address_code: code, line_1: line1, line_2: line2, city: city, region: region, country: country, postal_code: postal_code)
     end
   end
 end
