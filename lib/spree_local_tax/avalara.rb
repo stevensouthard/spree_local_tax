006f9ce0641c0af0f35e1e0a7e92c525fdc807e3
Josh Nussbaum <joshnuss@gmail.com>
2013-06-12 12:15:38 -0400

    Adds logic to add lines

--- lib/spree_local_tax/avalara.rb
+++ lib/spree_local_tax/avalara.rb
@@ -4,6 +4,10 @@ module SpreeLocalTax::Avalara
   def generate(order)
     generator = InvoiceGenerator.new
     generator.customer = order.email if order.email.present?
+    order.line_items.each do |line|
+      variant = line.variant
+      generator.add_line(variant.sku, variant.product.name, line.quantity, line.total)
+    end
     generator.invoice
   end
 end
--- spec/lib/avalara_spec.rb
+++ spec/lib/avalara_spec.rb
@@ -5,7 +5,7 @@ describe SpreeLocalTax::Avalara do
     let(:generator) { mock(:generator) }
 
     context "guest" do
-      let(:order) { mock(:order, email: nil) }
+      let(:order) { mock(:order, email: nil, line_items: []) }
 
       before do
         SpreeLocalTax::Avalara::InvoiceGenerator.should_receive(:new).and_return(generator)
@@ -19,12 +19,18 @@ describe SpreeLocalTax::Avalara do
     end
 
     context "user" do
-      let(:order) { mock(:order, email: 'wayne@gretzky.com') }
+      let(:product) { mock(:product, name: 'foo')}
+      let(:variant) { mock(:variant, sku: '1234', product: product)}
+      let(:line1)   { mock(:line, variant: variant, quantity: 2, total: 9.98) }
+      let(:line2)   { mock(:line, variant: variant, quantity: 3, total: 14.97) }
+      let(:order)   { mock(:order, email: 'wayne@gretzky.com', line_items: [line1, line2]) }
 
       before do
         SpreeLocalTax::Avalara::InvoiceGenerator.should_receive(:new).and_return(generator)
         generator.should_receive(:invoice).and_return(:invoice)
         generator.should_receive(:customer=).with('wayne@gretzky.com')
+        generator.should_receive(:add_line).with('1234', 'foo', 2, 9.98)
+        generator.should_receive(:add_line).with('1234', 'foo', 3, 14.97)
       end
 
       subject { SpreeLocalTax::Avalara.generate(order) }
