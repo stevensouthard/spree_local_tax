6cf4ec716ccea0750a08441c18642b531636b05b
Josh Nussbaum <joshnuss@gmail.com>
2013-06-12 16:38:26 -0400

    Adds destination address to builder

--- lib/spree_local_tax/avalara/invoice_builder.rb
+++ lib/spree_local_tax/avalara/invoice_builder.rb
@@ -1,13 +1,20 @@
 module SpreeLocalTax::Avalara
   class InvoiceBuilder
-    attr_reader :invoice
-
     def initialize
-      @invoice = Avalara::Request::Invoice.new
+      @invoice = ::Avalara::Request::Invoice.new
     end
 
     def customer=(value)
       @invoice.customer_code = value
+    end
+
+    def add_destination(line1, line2, city, region, country, postal_code)
+      @destination = ::Avalara::Request::Address.new(address_code: 'DESTINATION', line_1: line1, line_2: line2, city: city, region: region, country: country, postal_code: postal_code)
+    end
+
+    def invoice
+      @invoice.addresses = [@destination]
+      @invoice
     end
   end
 end
--- spec/lib/avalara/invoice_builder_spec.rb
+++ spec/lib/avalara/invoice_builder_spec.rb
@@ -3,15 +3,26 @@ require 'spec_helper'
 describe SpreeLocalTax::Avalara::InvoiceBuilder do
   let(:builder) { SpreeLocalTax::Avalara::InvoiceBuilder.new }
 
-  subject { builder.invoice }
+  context "#add_destination" do
+    before do
+      builder.customer = 'user@example.com'
+      builder.add_destination('123 Main St.', '#101', 'Toronto', 'ON', 'CA', 'H1H1H1')
+    end
 
-  context "#customer=" do
-    before { builder.customer = :name }
+    subject { builder.invoice }
 
-    its(:CustomerCode) { should == :name }
+    its(:CustomerCode) { should == 'user@example.com' }
+
+    context "destination" do
+      subject { builder.invoice.Addresses[0] }
+
+      its(:AddressCode) { should == 'DESTINATION' }
+      its(:Line1)       { should == '123 Main St.' }
+      its(:Line2)       { should == '#101' }
+      its(:City)        { should == 'Toronto' }
+      its(:Region)      { should == 'ON' }
+      its(:Country)     { should == 'CA' }
+      its(:PostalCode)  { should == 'H1H1H1' }
+    end
   end
-
-  context "#add_destination"
-  context "#add_origin"
-  context "#add_line"
 end
